dcc7201f38954189450f3e81e0cb0a644eddf195
MrTarantula <ryantarantula@gmail.com>
2018-07-02 09:01:51 -0500

    Updated test precision.

--- GamblersDice.Tests/GamblersDiceTests.cs
+++ GamblersDice.Tests/GamblersDiceTests.cs
@@ -13,19 +13,19 @@ namespace GamblersDice.Tests
         {
             var die = new GamblersDie(sides);
             int[] result = new int[sides];
-            int iters = 10_000_000;
+            decimal iters = 10_000_000M;
 
             for (int i = 0; i < iters; i++)
             {
                 result[die.Roll() - 1]++;
             }
 
-            double avg = iters / sides;
+            decimal avg = iters / sides;
 
             for (int i = 0; i < sides; i++)
             {
-                double roll = (result[i] - avg) / iters;
-                Assert.True(0.001 > roll, $"{roll} is outside of uniformity tolerance of 0.001");
+                decimal roll = (result[i] - avg) / iters;
+                Assert.True(0.001M > roll, $"{roll} is outside of uniformity tolerance of 0.001");
             }
         }
 
@@ -39,12 +39,8 @@ namespace GamblersDice.Tests
             Assert.Equal(expected, die.Roll());
         }
 
-        [Theory]
-        [InlineData(0.4, 0)]
-        [InlineData(0.3, 1)]
-        [InlineData(0.2, 2)]
-        [InlineData(0.1, 3)]
-        public void NonUniform_Inferred(double percent, int side)
+        [Fact]
+        public void NonUniform_Inferred()
         {
             // Given this state, [4,3,2,1]
             // 1 should be rolled 40% of the time
@@ -52,16 +48,19 @@ namespace GamblersDice.Tests
             // 3 should be rolled 20% of the time
             // 4 should be rolled 10% of the time
 
+            decimal iters = 1_000_000M;
             int[] result = new int[] { 0, 0, 0, 0 };
 
-            for (int run = 0; run < 1_000_000; run++)
+            for (int run = 0; run < iters; run++)
             {
                 var die = new GamblersDie(4, 3, 2, 1);
                 result[die.Roll() - 1]++;
             }
 
-            var tolerance = Math.Abs(percent - result[side] / 1_000_000D);
-            Assert.True(tolerance < 0.001, $"{tolerance} is outside uniformity tolerance of .001");
+            Assert.True(Math.Abs(0.4M - result[0] / iters) < 0.001M, $"Side one is outside of uniformity tolerance: {Math.Abs(0.4M - result[0] / iters)}");
+            Assert.True(Math.Abs(0.3M - result[1] / iters) < 0.001M, $"Side two is outside of uniformity tolerance: {Math.Abs(0.3M - result[1] / iters)}");
+            Assert.True(Math.Abs(0.2M - result[2] / iters) < 0.001M, $"Side three is outside of uniformity tolerance: {Math.Abs(0.2M - result[2] / iters)}");
+            Assert.True(Math.Abs(0.1M - result[3] / iters) < 0.001M, $"Side four is outside of uniformity tolerance: {Math.Abs(0.1M - result[3] / iters)}");
         }
     }
 }
